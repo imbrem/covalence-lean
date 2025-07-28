140eefba162dd206abe8be7dde8072e5c7f4aa7a
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-07-28 06:37:19 +0100

    The Plan

---
+++ Covalence/Uniqueness.lean
@@ -0,0 +1,17 @@
+import Covalence.HasTy
+
+-- Step 1 : define TyEq*
+
+-- Step 2 : define InnerTy and OuterTy
+
+-- Step 3 : hasty is TyEq* InnerTy
+
+-- Step 4 : hasty is TyEq* OuterTy
+
+-- Step 5 : OuterTy is hasty
+
+-- Step 6: hasty is TyEq* unique
+
+-- Step 7: TyEq is transitive, so TyEq* == TyEq
+
+-- Step 8: hasty is unique
